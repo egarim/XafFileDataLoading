9e621f5a2a2a1a914fd90d4b9c1c29974c6b9d33
Jose Manuel Ojeda Melgar <joche.ojeda@bitframeworks.com>
2022-06-20 09:02:34 -0700

    Add project files.

---
+++ XafFileDataLoading.Module.Win/Controllers/Class1.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.FileAttachments.Win;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XafFileDataLoading.Module.BusinessObjects;
+
+namespace XafFileDataLoading.Module.Win.Controllers
+{
+    public class MyViewController :ObjectViewController<DetailView, DomainObject1>
+    {
+        public MyViewController() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            var FileDataEditor=this.View.FindItem("File");
+            var FileDataEditorControl=FileDataEditor.Control as FileDataEdit;
+            FileDataEditorControl.Modified += FileDataEditorControl_Modified;
+            // Access and customize the target View control.
+        }
+
+        private void FileDataEditorControl_Modified(object sender, EventArgs e)
+        {
+            var CurrentFileDataEdit=  sender as FileDataEdit;
+            if(CurrentFileDataEdit.FileData.Size>0)
+            {
+                MemoryStream stream = new MemoryStream();
+                CurrentFileDataEdit.FileData.SaveToStream(stream);
+                var ContentLengt = stream.Length;
+            }
+        }
+    }
+}
